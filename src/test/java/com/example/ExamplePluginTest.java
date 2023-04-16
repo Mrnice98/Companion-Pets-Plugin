451ea25937eec0e22065681588f9c0466207619d
Mrnice <ckbenedetti@gmail.com>
2023-04-16 18:17:02 -0400

    remove shadow jar code

--- src/test/java/com/example/ExamplePluginTest.java
+++ src/test/java/com/example/ExamplePluginTest.java
@@ -3,41 +3,11 @@ package com.example;
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-import java.io.File;
-
 
 public class ExamplePluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		boolean assertions = false;
-		assert assertions = true;
-
-		if (!assertions)
-		{
-
-			String selfPath = new File(
-					RuneLite.class
-							.getProtectionDomain()
-							.getCodeSource()
-							.getLocation()
-							.toURI()
-			).getAbsolutePath();
-
-
-			//need to set base lang to java 11
-//			String jCmd = ProcessHandle.current()
-//					.info()
-//					.command()
-//					.orElse("java");
-
-
-			new ProcessBuilder()
-					.inheritIO()
-					.command("java", "-ea", "-jar", selfPath, "--developer-mode")
-					.start();
-			return;
-		}
 
 		ExternalPluginManager.loadBuiltin(CompanionPetPlugin.class);
 
